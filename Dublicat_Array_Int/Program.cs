26b3fa0e91d2a0b084b3e24a9860595a26203dec
Albert Hakobyan <abhakobyan24@gmail.com>
2021-08-27 11:57:29 +0400

    Добавьте файлы проекта.

---
+++ Dublicat_Array_Int/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dublicat_Array_Int
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 1, 1, 2, 2, 2, 6 };
+            int n = CountDuplicatePairs(array);
+            Console.WriteLine(n);
+        }
+
+        static int CountDuplicatePairs(int[] arr)
+        {
+            int temp = 0;
+            int count = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == 0)
+                {
+                    temp++;
+                }
+            }
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] == arr[j] && arr[j] != 0)
+                    {
+                        count++;
+                        arr[i] = 0;
+                        arr[j] = 0;
+                        continue;
+                    }
+                }
+            }
+            return count + temp / 2;
+        }
+    }
+}
